e2080987980fb2726bf45b4f9d91ad5e94efbf0c
zulfadli <zulfadlisimatupang@ecampus.ut.ac.id>
2023-12-12 09:55:23 +0700

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        neutral: "#F1F4F5",
+      },
+    },
+  },
+  plugins: [],
+};
